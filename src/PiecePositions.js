a336f08638aac558a3f1cf7766baf9a4ace78954
Ben <ben.costolo@gmail.com>
2017-02-09 14:35:44 -0500

    experiment with storing the positions of all pieces within a centralized object

---
+++ src/PiecePositions.js
@@ -0,0 +1,18 @@
+export default pieces = {
+  knight: [
+    {
+      color: 'white',
+      positions: [
+        [1, 7],
+        [6, 7]
+      ]
+    },
+    {
+      color: 'black',
+      positions: [
+        [2, 0],
+        [6, 0]
+      ]
+    }
+  ]
+}
