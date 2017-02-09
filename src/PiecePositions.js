0cdbf33c2cde603486bb7a50f5da1cc9dc370d9a
Ben <ben.costolo@gmail.com>
2017-02-09 15:12:26 -0500

    refactor PiecePositions

--- src/PiecePositions.js
+++ src/PiecePositions.js
@@ -1,18 +1,12 @@
-export default pieces = {
-  knight: [
-    {
-      color: 'white',
-      positions: [
-        [1, 7],
-        [6, 7]
-      ]
-    },
-    {
-      color: 'black',
-      positions: [
-        [2, 0],
-        [6, 0]
-      ]
-    }
-  ]
+export const pieces = {
+  knight: {
+    white: [
+      [1, 7],
+      [6, 7]
+    ],
+    black: [
+      [2, 0],
+      [6, 0]
+    ]
+  }
 }
